bd888866eb6a557d203bd42019ec1f60d2544d0b
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-05-06 12:28:45 -0400

    initial commit

---
+++ controllers/restaurant.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+require('dotenv').config();
+
+const handleApiCall = (req, res ) => {
+  axios
+  .get(`https://api.yelp.com/v3/businesses/search`, {
+    headers: {
+        Authorization: `Bearer ${process.env.YELP_API_KEY}`
+    },
+    params: {
+      location: `${req.body.input}`
+    }    
+  })  
+    .then(data => {
+      res.json(data.data.businesses);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+  
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
