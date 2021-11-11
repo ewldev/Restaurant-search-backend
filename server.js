7e2b199460827ec3461cf882ba4be5fb2c9d8db6
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-11-11 14:27:32 -0500

    disabled sign in, register & profile

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/restaurant');
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'admin',
-    database : 'smart-brain'
-  }
-});
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host : '127.0.0.1',
+//     user : 'postgres',
+//     password : 'admin',
+//     database : 'smart-brain'
+//   }
+// });
 
 const app = express();
 
@@ -24,9 +24,9 @@ app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res)=> {res.send("LOOK ITS WORKING") })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+// app.post('/signin', signin.handleSignin(db, bcrypt))
+// app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+// app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.post('/search', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(3000, ()=> {
