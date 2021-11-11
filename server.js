6c8767f4f34ace952adfeba4ac390398afa88077
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-11-11 15:08:26 -0500

    commented out unused const server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const bcrypt = require('bcrypt-nodejs');
+// const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex');
+// const knex = require('knex');
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
+// const register = require('./controllers/register');
+// const signin = require('./controllers/signin');
+// const profile = require('./controllers/profile');
 const image = require('./controllers/restaurant');
 
 // const db = knex({
